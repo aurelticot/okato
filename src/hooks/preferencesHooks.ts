6b847070910ac0c4e779c62e69ea4e6ce671ee43
Aurel <aurelticot.dev@gmail.com>
2020-08-29 18:41:04 +1000

    handle undefined preference

--- src/hooks/preferencesHooks.ts
+++ src/hooks/preferencesHooks.ts
@@ -4,21 +4,23 @@ import SettingKeys from "enums/SettingKeys";
 import PreferenceDefinition from "interfaces/Setting";
 import config from "config";
 
-const { settings } = config;
+const { settings, defaultPreferenceValues } = config;
 
 export function usePreference(key: SettingKeys) {
   const { preferences, setPreference } = useContext(PreferenceContext);
   const definition = settings[key];
+  const defaultValue = defaultPreferenceValues[key];
   const preference = preferences[key];
-  // TODO handle undefined preference
+
   function setter(value: string | string[]): void {
     setPreference(key, value);
   }
 
-  const returnedObject: [string | string[], (value: string | string[]) => void, PreferenceDefinition] = [
-    preference,
-    setter,
-    definition,
-  ];
+  const returnedObject: [
+    string | string[],
+    (value: string | string[]) => void,
+    PreferenceDefinition,
+    string | string[]
+  ] = [preference ? preference : defaultValue, setter, definition, defaultValue];
   return returnedObject;
 }
